493af0e2cc93b0fefd082d9f78fc1d1a399b3fbc
Lucas Poirel <lpoirel@sqli.com>
2022-11-23 15:25:27 +0100

    add basic todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hola chicos
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+
+const App = () => {
+	return (
+		<div className="app">
+			<h1 className="app-title">My Tasks</h1>
+			<AddTodo />
+			<TodoList />
+		</div>
+	);
+};
 
 export default App;
